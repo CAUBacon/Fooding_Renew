1586433cde11f241e1854ca29cb64684a94a5a99
JongwonYoun <jeffyoun@naver.com>
2021-06-06 16:34:20 +0900

    refac: 필요없는 주석 삭제

--- src/main/java/teaspoon/fooding/api/config/SwaggerConfiguration.java
+++ src/main/java/teaspoon/fooding/api/config/SwaggerConfiguration.java
@@ -10,7 +10,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,7 +20,6 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
-        System.out.println("SwaggerConfiguration.api");
         return new Docket(DocumentationType.SWAGGER_2)
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
