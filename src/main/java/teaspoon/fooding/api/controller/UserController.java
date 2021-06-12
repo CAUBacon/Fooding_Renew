2fc35674e4c5c3d647398add4ab49bc62f6dc1a8
JongwonYoun <jeffyoun@naver.com>
2021-06-12 20:39:13 +0900

    refac: 사용자와 가게 상호 작용에 대한 컨트롤러 분리

--- src/main/java/teaspoon/fooding/api/controller/UserController.java
+++ src/main/java/teaspoon/fooding/api/controller/UserController.java
@@ -1,66 +1,12 @@
 package teaspoon.fooding.api.controller;
 
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-import teaspoon.fooding.api.dto.ShopLikeCountResult;
-import teaspoon.fooding.api.dto.SingleResult;
-import teaspoon.fooding.domain.shop.Shop;
-import teaspoon.fooding.domain.user.User;
-import teaspoon.fooding.domain.user.UserRole;
-import teaspoon.fooding.service.ResponseService;
-import teaspoon.fooding.service.ShopService;
-import teaspoon.fooding.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
 
-    private final UserService userService;
-    private final ShopService shopService;
-    private final ResponseService responseService;
-
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "x-auth-token", value = "로그인 토큰", required = true, dataType = "String",
-                    paramType = "header")
-    })
-    @ApiOperation(value = "사용자가 가게를 좋아요 누르는 엔드포인트", notes = "가게 좋아요")
-    @Secured(UserRole.ROLE.COMMON)
-    @PostMapping("/shop/{shopId}/like")
-    public ResponseEntity<SingleResult<ShopLikeCountResult>> likeShop(@AuthenticationPrincipal User user,
-                                                                      @ApiParam(value = "가게 id", required = true) @PathVariable Long shopId) {
-        Shop shop = shopService.findShopById(shopId);
-        long likeCount = userService.likeShop(user, shop);
-        ShopLikeCountResult result = ShopLikeCountResult.builder()
-                .shopId(shopId)
-                .likeCount(likeCount)
-                .build();
-        return responseService.makeSingleResponse(result, HttpStatus.CREATED, 201);
-    }
-
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "x-auth-token", value = "로그인 토큰", required = true, dataType = "String",
-                    paramType = "header")
-    })
-    @ApiOperation(value = "사용자가 가게를 좋아요 취소하는 엔드포인트", notes = "가게 좋아요 취소")
-    @Secured(UserRole.ROLE.COMMON)
-    @DeleteMapping("/shop/{shopId}/like")
-    public ResponseEntity<SingleResult<ShopLikeCountResult>> unlikeShop(@AuthenticationPrincipal User user,
-                                                                        @ApiParam(value = "가게 id", required = true) @PathVariable Long shopId) {
-        Shop shop = shopService.findShopById(shopId);
-        long likeCount = userService.unlikeShop(user, shop);
-        ShopLikeCountResult result = ShopLikeCountResult.builder()
-                .shopId(shopId)
-                .likeCount(likeCount)
-                .build();
-        return responseService.makeSingleResponse(result, HttpStatus.CREATED, 201);
-    }
 }
