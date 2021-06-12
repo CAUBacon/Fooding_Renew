7ac6a6309ff73e0b63cb3da7ba05e64226722faf
JongwonYoun <jeffyoun@naver.com>
2021-06-12 17:16:44 +0900

    feat: 가게 좋아요 취소 엔드포인트 추가

--- src/main/java/teaspoon/fooding/api/controller/UserController.java
+++ src/main/java/teaspoon/fooding/api/controller/UserController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import teaspoon.fooding.api.dto.ShopLikeCountResult;
 import teaspoon.fooding.api.dto.SingleResult;
 import teaspoon.fooding.domain.shop.Shop;
@@ -39,6 +36,23 @@ public class UserController {
                                                                       @ApiParam(value = "가게 id", required = true) @PathVariable Long shopId) {
         Shop shop = shopService.findShopById(shopId);
         long likeCount = userService.likeShop(user, shop);
+        ShopLikeCountResult result = ShopLikeCountResult.builder()
+                .shopId(shopId)
+                .likeCount(likeCount)
+                .build();
+        return responseService.makeSingleResponse(result, HttpStatus.CREATED, 201);
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "x-auth-token", value = "로그인 토큰", required = true, dataType = "String",
+                    paramType = "header")
+    })
+    @ApiOperation(value = "사용자가 가게를 좋아요 취소하는 엔드포인트", notes = "가게 좋아요 취소")
+    @DeleteMapping("/shop/{shopId}/like")
+    public ResponseEntity<SingleResult<ShopLikeCountResult>> unlikeShop(@AuthenticationPrincipal User user,
+                                                                        @ApiParam(value = "가게 id", required = true) @PathVariable Long shopId) {
+        Shop shop = shopService.findShopById(shopId);
+        long likeCount = userService.unlikeShop(user, shop);
         ShopLikeCountResult result = ShopLikeCountResult.builder()
                 .shopId(shopId)
                 .likeCount(likeCount)
